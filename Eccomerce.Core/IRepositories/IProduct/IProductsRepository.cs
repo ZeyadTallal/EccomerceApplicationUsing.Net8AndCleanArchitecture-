d933b6522b1c583537f8c1dd914a91cc59464a01
zeyadtallal <zeyadtallalmoharam@gmail.com>
2024-05-26 01:47:31 +0300

    Pagination And Filteration And Sorting

--- Eccomerce.Core/IRepositories/IProduct/IProductsRepository.cs
+++ Eccomerce.Core/IRepositories/IProduct/IProductsRepository.cs
@@ -1,10 +1,11 @@
-﻿using Ecommerce.Core.Entities.Products;
+﻿using Ecommerce.Core.Constants;
+using Ecommerce.Core.Entities.Products;
 
 namespace Ecommerce.Core.IRepositories.IProduct
 {
     public interface IProductsRepository
     {
-        Task<IEnumerable<Product>> GetAllAsync();
+		Task<(IEnumerable<Product>, int)> GetAllAsync(string? Keyword , int pageSize , int pageNumber , string? sortBy , SortDirection sortDirection);
         Task<Product> GetByIdAsync(int id);
         Task<int> Create(Product product);
         bool IsNameExists(string name);
